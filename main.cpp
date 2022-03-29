a72aad3e318b28531c52dc65f4adc0de1ee30370
Javier.Nieves <Javier.Nieves@SEV1-VQI0REEWQM>
2022-03-29 08:11:37 -0600

    updated file push

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include "degree.h"
+#include "student.h"
+#include "roster.h"
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	cout << "Course: Scripting and Programming Applications - C867" << endl;
+    cout << "Language: C++" << endl;
+    cout << "StudentID: 001548573" << endl;
+	cout << "Student Name: Javier Nieves" << endl << endl << endl;
+
+    const string studentData[] = {
+        "A1,John,Smith,John1989@gm ail.com,20,30,35,40,SECURITY",
+        "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
+        "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
+        "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
+        "A5,Dan,Adams,DanAdamsHarder@Outlook.com,34,21,32,44,SOFTWARE",
+        "A6,Javier,Nieves,drcappuchino2@gmail.com,27,20,31,43,SOFTWARE"
+    };
+
+    const int studentNum = sizeof(studentData) / sizeof(studentData[0]);
+
+    Roster classRoster;
+    for (int i = 0; i < studentNum; i++) {
+        classRoster.Parse(studentData, i);
+
+    }
+
+    classRoster.PrintAll();
+    classRoster.PrintInvalidEmail();
+
+    //Print Average Days in Course
+    for (int i = 0; i < studentNum; i++) {
+        classRoster.PrintDaysInCourse(classRoster.classRosterArray.at(i)->getStudentID());
+    }
+    cout << endl;
+    classRoster.PrintByDegree(DegreeProgram::SOFTWARE);
+    cout << endl;
+
+    classRoster.Remove("A3");
+    classRoster.PrintAll();
+    classRoster.Remove("A3");
+
+    return 0;
+}
---
+++ student.cpp
@@ -0,0 +1,87 @@
+#include "degree.h"
+#include "student.h"
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+string stringDegreeProgram[] = { "SECURITY", "NETWORK", "SOFTWARE", "BLANK" };
+
+Student::Student(string studentID, string firstName, string lastName, string emailAddr, int studentAge, vector<int> daysInCourse, DegreeProgram degreeProgram)
+{
+					this->studentID = studentID;
+					this->firstName = firstName;
+					this->lastName = lastName;
+					this->emailAddr = emailAddr;
+					this->studentID = studentAge;
+					this->daysInCourse = daysInCourse;
+					this->degreeProgram = degreeProgram;
+}
+
+Student::Student()
+{
+	this->studentID = "";
+	this->firstName = "";
+	this->lastName = "";
+	this->emailAddr = "";
+	this->studentAge = 99;
+	this->daysInCourse = { 0,0,0 };
+	this->degreeProgram = DegreeProgram::BLANK;
+}
+
+//Setter Fucntions
+void Student::setStudentID(string studentID) { this->studentID = studentID; }
+void Student::setFirstName(string firstName) { this->firstName = firstName; }
+void Student::setLastName(string lastName) { this->lastName = lastName; }
+void Student::setEmailAddr(string emailAddr) { this->emailAddr = emailAddr; }
+void Student::setStudentAge(int studentAge) { this->studentAge = studentAge; }
+void Student::setDaysInCourse(vector<int> daysInCourse) { this->daysInCourse = daysInCourse; }
+void Student::setDegreeProgram(DegreeProgram degreeProgram) { this->degreeProgram = degreeProgram; }
+
+//Getter Functions
+string Student::getStudentID() const { return studentID; }
+string Student::getFirstName() const { return firstName; }
+string Student::getLastName() const { return lastName; }
+string Student::getEmailAddr() const { return emailAddr; }
+int Student::getStudentAge() const { return studentAge; }
+vector<int> Student::getDaysInCourse() const { return daysInCourse; }
+DegreeProgram Student::getDegreeProgram() const { return degreeProgram; }
+
+string Student::getDaysInCourseString() const
+{
+	stringstream ss;
+	for (int i = 0; i < daysInCourse.size(); i++) {
+		if (i != 0)
+			ss << ",";
+		ss << daysInCourse[i];
+	}
+	string sDays = ss.str();
+	return sDays;
+}
+
+string Student::getDegreeProgramString() const
+{
+	string sDegree;
+	if (degreeProgram == DegreeProgram::SECURITY) {
+		sDegree = stringDegreeProgram[(int)degreeProgram];
+	}
+	else if (degreeProgram == DegreeProgram::NETWORK) {
+		sDegree = stringDegreeProgram[(int)degreeProgram];
+	}
+	else if (degreeProgram == DegreeProgram::SOFTWARE) {
+		sDegree = stringDegreeProgram[(int)degreeProgram];
+	}
+	return sDegree;
+}
+
+void Student::Print() {
+	cout << "Student ID: " << getStudentID() << "\t";
+	cout << "First Name: " << getFirstName() << "\t";
+	cout << "Last Name: " << getLastName() << "\t";
+	cout << "Email Address: " << getEmailAddr() << "\t";
+	cout << "Age: " << getStudentAge() << "\t";
+	cout << "Days In Course: " << getDaysInCourseString() << "\t";
+	cout << "Degree Program: " << getDegreeProgramString() << endl;
+}
+
+Student::~Student() {} 
